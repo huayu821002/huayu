aa29ae51e91400e07fa9080c40356d3d7b61a860
Fiestaflare Dev <dev@fiestaflare.com>
2026-07-25 17:30:00 +0800

    fix: syntax error missing closing bracket

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -501,7 +501,7 @@ function ProductsContent() {
             onClear={clearBatchSelection}
             onContinueShopping={() => setBatchMode(false)}
           />
-        )
+        )}
       </div>
     </>
   )
